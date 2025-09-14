2b222f1eb374a3a46e2b272044fc46d75cfcc05e
Dzaki <dzaki.novazka.putra0@gmail.com>
2025-09-14 17:49:00 +0700

    Delete .invisible-text.py

--- .invisible-text.py
+++
@@ -1,9 +0,0 @@
-import os,sys
-input = input ("> ")
-os.system("sleep 9.9") 
-os.system("clear") 
-print("\n\r") 
-os.system("clear") 
-os.system("sleep 9.9") 
-os.system("clear") 
-os.system("python .auto-clear.py") 
